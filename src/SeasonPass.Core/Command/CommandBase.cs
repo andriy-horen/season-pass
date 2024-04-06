888e23ff4d3bbc254cf70fc4b8ecf2088eb1c613
Andriy Horen <go.andriy@gmail.com>
2024-04-06 15:37:54 +0200

    Add CommandBase class

---
+++ src/SeasonPass.Core/Command/CommandBase.cs
@@ -0,0 +1,16 @@
+﻿namespace SeasonPass.Core.Command;
+
+public abstract class CommandBase<TResult> : ICommand<TResult>
+{
+    public Guid Id { get; }
+
+    protected CommandBase()
+    {
+        this.Id = Guid.NewGuid();
+    }
+
+    protected CommandBase(Guid id)
+    {
+        this.Id = id;
+    }
+}
