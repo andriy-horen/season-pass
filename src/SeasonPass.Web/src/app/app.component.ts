690c10318fd464a307144060abade109a469a0f4
Andriy Horen <go.andriy@gmail.com>
2024-04-11 23:17:57 +0200

    Clean up app component

--- src/SeasonPass.Web/src/app/app.component.ts
+++ src/SeasonPass.Web/src/app/app.component.ts
@@ -1,25 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
-import { AutoCompleteModule } from 'primeng/autocomplete';
-import { ButtonModule } from 'primeng/button';
-import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    ButtonModule,
-    AutoCompleteModule,
-    FormsModule,
-    CheckboxModule,
-  ],
+  imports: [RouterOutlet],
 })
 export class AppComponent implements OnInit {
   constructor(private primengConfig: PrimeNGConfig) {}
@@ -29,7 +17,4 @@ export class AppComponent implements OnInit {
   }
 
   title = 'season-pass';
-  suggestions = [];
-  selectedItem = '';
-  pizza = null;
 }
